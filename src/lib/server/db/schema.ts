0d9b33adad2fb7a8c6244d8ae1f97444e8c9cf3f
Taylor Lohman <taylorlohman@duck.com>
2026-02-18 18:39:44 -0800

    schema prototype

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,23 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { integer, sqliteTable, text, real } from 'drizzle-orm/sqlite-core';
 
-export const task = sqliteTable('task', {
-	id: text('id')
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
+export const reports = sqliteTable('reports', {
+  id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+  // reporter info
+  reporter_email: text('reporter_email').notNull(),
+  // vehicle info
+	license_plate: text('license_plate').notNull(),
+  plate_state: text('plate_state').notNull(),
+  vehicle_make: text('vehicle_make').notNull(),
+  vehicle_model: text('vehicle_model'),
+  vehicle_color: text('vehicle_color').notNull(),
+  // location 
+  latitude: real('latitude'),
+  longitude: real('longitude'),
+  // details -- '72 hours', 'expired tags', 'other'
+  reason: text('reason').notNull(),
+  notes: text('notes'),
+  // track status -- 'pending', 'submitted to ciy', 'resolved', 'dismissed'
+  status: text('status').notNull().default('pending')
+	created_at: integer('created_at', { mode: 'timestamp' }).$defaultFn(() => new Date()),
+  updated_at: integer('updated_at', { mode: 'timestamp' }).$defaultFn(() => new Date()),
 });
