f8fbecd6a57c100a1d3073cb87a2f74a633dd40e
Taylor Lohman <taylorlohman@duck.com>
2026-02-26 07:35:20 -0800

    removed manual

--- src/lib/components/VehicleSelector.svelte
+++ src/lib/components/VehicleSelector.svelte
@@ -101,152 +101,101 @@
 </script>
 
 <div class="vehicle-selector">
-  <!-- Manual Entry Toggle -->
-  <label class="manual-toggle">
-    <input type="checkbox" bind:checked={manualEntry} onchange={toggleManual} />
-    <span>Can't find your vehicle? Enter manually</span>
-  </label>
-
-  {#if manualEntry}
-    <!-- Manual Entry Mode -->
-    <div class="form-row">
-      <div class="form-group">
-        <label for="manual-make">Vehicle Make *</label>
+  <div class="form-row">
+    <!-- Make Selector -->
+    <div class="form-group autocomplete-container">
+      <label for="make-input">Vehicle Make *</label>
+      <div class="input-wrapper">
         <input
-          id="manual-make"
+          id="make-input"
           type="text"
-          bind:value={selectedMake}
-          placeholder="e.g., Honda"
+          bind:value={makeQuery}
+          onkeydown={handleMakeKeydown}
+          onfocus={() => {
+            makeInputTouched = true; // Mark as touched
+            // Only show if we haven't selected yet or are changing selection
+            if (makeQuery !== selectedMake) {
+              showMakeDropdown = true;
+            }
+          }}
+          onblur={() => {
+            setTimeout(() => {
+              showMakeDropdown = false;
+            }, 200);
+          }}
+          placeholder="Start typing... (e.g., Honda)"
+          autocomplete="off"
         />
+        <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
       </div>
 
-      <div class="form-group">
-        <label for="manual-model">Vehicle Model *</label>
+      {#if showMakeDropdown && filteredMakes.length > 0}
+        <div class="dropdown">
+          {#each filteredMakes as make}
+            <button
+              type="button"
+              class="dropdown-item"
+              onclick={() => selectMake(make)}
+            >
+              {make}
+            </button>
+          {/each}
+        </div>
+      {/if}
+    </div>
+
+    <!-- Model Selector -->
+    <div class="form-group autocomplete-container">
+      <label for="model-input">Vehicle Model *</label>
+      <div class="input-wrapper">
         <input
-          id="manual-model"
+          id="model-input"
           type="text"
-          bind:value={selectedModel}
-          placeholder="e.g., Civic"
+          bind:value={modelQuery}
+          onkeydown={handleModelKeydown}
+          onfocus={() => {
+            modelInputTouched = true; // Mark as touched
+            // Only show if we have a make selected and haven't selected a model yet
+            if (selectedMake && modelQuery !== selectedModel) {
+              showModelDropdown = true;
+            }
+          }}
+          onblur={() => {
+            setTimeout(() => {
+              showModelDropdown = false;
+            }, 200);
+          }}
+          placeholder={selectedMake ? 'Start typing...' : 'Select make first'}
+          autocomplete="off"
+          disabled={!selectedMake}
         />
+        <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
       </div>
-    </div>
-  {:else}
-    <!-- Autocomplete Mode -->
-    <div class="form-row">
-      <!-- Make Selector -->
-      <div class="form-group autocomplete-container">
-        <label for="make-input">Vehicle Make *</label>
-        <div class="input-wrapper">
-          <input
-            id="make-input"
-            type="text"
-            bind:value={makeQuery}
-            onkeydown={handleMakeKeydown}
-            onfocus={() => {
-              makeInputTouched = true; // Mark as touched
-              // Only show if we haven't selected yet or are changing selection
-              if (makeQuery !== selectedMake) {
-                showMakeDropdown = true;
-              }
-            }}
-            onblur={() => {
-              setTimeout(() => {
-                showMakeDropdown = false;
-              }, 200);
-            }}
-            placeholder="Start typing... (e.g., Honda)"
-            autocomplete="off"
-          />
-          <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-          </svg>
+
+      {#if showModelDropdown && filteredModels.length > 0}
+        <div class="dropdown">
+          {#each filteredModels as model}
+            <button
+              type="button"
+              class="dropdown-item"
+              onclick={() => selectModel(model)}
+            >
+              {model}
+            </button>
+          {/each}
         </div>
-
-        {#if showMakeDropdown && filteredMakes.length > 0}
-          <div class="dropdown">
-            {#each filteredMakes as make}
-              <button
-                type="button"
-                class="dropdown-item"
-                onclick={() => selectMake(make)}
-              >
-                {make}
-              </button>
-            {/each}
-          </div>
-        {/if}
-      </div>
-
-      <!-- Model Selector -->
-      <div class="form-group autocomplete-container">
-        <label for="model-input">Vehicle Model *</label>
-        <div class="input-wrapper">
-          <input
-            id="model-input"
-            type="text"
-            bind:value={modelQuery}
-            onkeydown={handleModelKeydown}
-            onfocus={() => {
-              modelInputTouched = true; // Mark as touched
-              // Only show if we have a make selected and haven't selected a model yet
-              if (selectedMake && modelQuery !== selectedModel) {
-                showModelDropdown = true;
-              }
-            }}
-            onblur={() => {
-              setTimeout(() => {
-                showModelDropdown = false;
-              }, 200);
-            }}
-            placeholder={selectedMake ? 'Start typing...' : 'Select make first'}
-            autocomplete="off"
-            disabled={!selectedMake}
-          />
-          <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-          </svg>
-        </div>
-
-        {#if showModelDropdown && filteredModels.length > 0}
-          <div class="dropdown">
-            {#each filteredModels as model}
-              <button
-                type="button"
-                class="dropdown-item"
-                onclick={() => selectModel(model)}
-              >
-                {model}
-              </button>
-            {/each}
-          </div>
-        {/if}
-      </div>
+      {/if}
     </div>
-  {/if}
+  </div>
 </div>
 
 <style>
   .vehicle-selector {
     width: 100%;
-  }
-
-  .manual-toggle {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-    margin-bottom: 1rem;
-    font-size: 0.875rem;
-    color: #6b7280;
-    cursor: pointer;
-    user-select: none;
-  }
-
-  .manual-toggle input[type="checkbox"] {
-    cursor: pointer;
-  }
-
-  .manual-toggle:hover span {
-    color: #3b82f6;
   }
 
   .form-row {
